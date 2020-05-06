5f6916fe15c1cce05dbfaecd786633ed64d071ee
Aaron <aaronkh19@gmail.com>
2020-05-05 19:57:07 -0600

    added age verification to the master branch

---
+++ assets/ageCheck/ageCheck.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".ageCheck").on("click", function () {
+    console.log("test click");
+  });
+});
+
+//Things to do -
+// Make it so that if the user is under 21 they get redirected to a kid friendly page
+//Check to see if the age is legal, if so then let the user access the page
+//All that will run once the user hits the verify button
+//Set it to redirect the user if they try to skip the age verification process
+//Only allow the user to view the page once its confiremed they are 21
