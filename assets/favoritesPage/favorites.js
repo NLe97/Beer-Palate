e137bac2f9530842d2dd3a7395b9801b676be40b
Mason <Mudland2721@gamil.com>
2020-05-05 19:30:41 -0600

    added favoritespage folder

---
+++ assets/favoritesPage/favorites.js
@@ -0,0 +1,23 @@
+$('button').click(function () {
+    location.href = "https://www.lyft.com/rider"
+
+})
+
+
+// $( document ).ready(function() {
+
+
+
+
+
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       }).then(function(response) {
+//         console.log(response);
+//       });
+
+
+
+// });
